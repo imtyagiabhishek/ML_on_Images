c6ee9a83f992913ed07e067ffb491960446b7f76
imtygaiabhishek <imabhishektyagi@outlook.com>
2021-08-01 13:25:10 +0530

    Initial commit

---
+++ lib/VisionDetectorViews/detector_views.dart
@@ -0,0 +1,5 @@
+export 'barcode_scanner_view.dart';
+export 'digital_ink_recogniser_view.dart';
+export 'face_detector_view.dart';
+export 'label_detector_view.dart';
+export 'text_detector_view.dart';
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'VisionDetectorViews/barcode_scanner_view.dart';
+import 'VisionDetectorViews/digital_ink_recogniser_view.dart';
+import 'VisionDetectorViews/face_detector_view.dart';
+import 'VisionDetectorViews/label_detector_view.dart';
+import 'VisionDetectorViews/text_detector_view.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google ML Kit Demo App'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  CustomCard(
+                    'Image Label Detector',
+                    ImageLabelView(),
+                    featureCompleted: true,
+                  ),
+                  CustomCard(
+                    'Face Detector',
+                    FaceDetectorView(),
+                    featureCompleted: true,
+                  ),
+                  CustomCard(
+                    'Barcode Scanner',
+                    BarcodeScannerView(),
+                    featureCompleted: true,
+                  ),
+                  CustomCard('Digital Ink Recogniser', DigitalInkView()),
+                  CustomCard(
+                    'Text Detector',
+                    TextDetectorView(),
+                    featureCompleted: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
+
+  const CustomCard(this._label, this._viewPage,
+      {this.featureCompleted = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (Platform.isIOS && !featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                    'This feature has not been implemented for iOS yet')));
+          } else
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
+        },
+      ),
+    );
+  }
+}
